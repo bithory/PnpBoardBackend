65aa871d08b5510a16fe33e1eb8543efd4763cf3
Diana Fechner <Diana.Fechner@fh-erfurt.de>
2019-03-10 17:42:25 +0100

    added the tag module

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
  * Time: 19:09
  */
 
+$dir = './modules/';
+
+require_once ($dir . 'navigation/navigation.php');
+require_once ($dir . 'worlds/worlds.php');
+require_once ($dir . 'parties/parties.php');
+require_once ($dir . 'notes/notes.php');
+require_once ($dir . 'tags/tags.php');
+
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -16,42 +24,35 @@ class Router
 	public function __construct()
 	{
 
-//		$test = array();
-//
-//		$test['status'] = 'HTTP OK';
-//		$test['text']   = 'and this is an example text';
-//
-//		echo json_encode($test);
-
 		$module         = $_GET['module'];
 		$action         = $_GET['action'];
 		$data           = $_GET['data'];
 
 		$result         = null;
 
-		$dir = './modules/';
 
 		switch($module){
 
 			case 'navigation':
-				require_once ($dir . 'navigation/navigation.php');
 				$navigation = new Navigation();
 				$result = $navigation->dbAction($action, $data);
 				break;
 			case 'worlds':
-				require_once ($dir . 'worlds/worlds.php');
 				$worlds = new Worlds();
 				$result = $worlds->dbAction($action, $data);
 				break;
 			case 'parties':
-				require_once ($dir . 'parties/parties.php');
 				$parties    = new Parties();
 				$result     = $parties->dbAction($action, $data);
 				break;
 			case 'notes':
-				require_once ($dir . 'notes/notes.php');
 				$notes  = new Notes();
 				$result = $notes->dbAction($action, $data);
+				break;
+			case 'tags':
+				$tags   = new Tags();
+				$result = $tags->dbAction($action, $data);
+				break;
 		}
 
 		$this->returnData($result);
@@ -59,12 +60,6 @@ class Router
 
 	private function returnData($res){
 
-//		$test = array();
-//
-//		$test['status'] = 'HTTP OK';
-//		$test['text']   = 'and this is an example text';
-
-//		echo var_dump($res);
 		echo json_encode($res);
 	}
 }
---
+++ modules/tags/tags.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dianafechner
+ * Date: 10.03.2019
+ * Time: 15:07
+ */
+
+class Tags
+{
+	/**
+	 * @var DBConnection
+	 */
+	private $dbConn;
+
+	private $table;
+
+	public function __construct(){
+
+		$this->table = 'mod_tag';
+	}
+
+	public function dbAction(&$action, &$data){
+
+		$this->dbConn = new DBConnection();
+
+		$result = false;
+
+		switch($action){
+			case 'index':
+				$result = $this->getIndex($data);
+				break;
+			case 'add':
+				$result = $this->addData($data);
+				break;
+			case 'edit':
+				$result = $this->editData($data);
+				break;
+			case 'load':
+				$result = $this->loadData($data);
+				break;
+			case 'delete':
+				$result = $this->deleteData($data);
+				break;
+		}
+
+		$this->dbConn->db->close();
+
+		return $result;
+	}
+
+	/**
+	 * Diese Funktion Holt die Liste für die Index Ansicht
+	 *
+	 * @return array|bool
+	 */
+	private function getIndex(Array &$data){
+
+		$sql = 'SELECT * FROM ' . $this->table . ' WHERE party_id = ' . $data['id'] . ' ORDER BY id DESC';
+
+		$tmpRes = $this->dbConn->db->query($sql);
+
+		$result = $this->dbConn->mysqliToData($tmpRes);
+
+		return $result;
+	}
+
+
+	private function loadData(Array &$data){
+
+		$sql = 'SELECT * FROM ' . $this->table . ' WHERE id = ' . $data['id'];
+
+		$tmpRes = $this->dbConn->db->query($sql);
+
+		$result = $this->dbConn->mysqliToData($tmpRes);
+		$this->dbConn->convertKey($result);
+
+		return $result[0];
+	}
+
+	private function addData(Array &$data){
+
+		$sql = 'INSERT INTO ' . $this->table .' (name, party_id) VALUES ( ';
+		$sql .= "'" . $data['name']     . "', ";
+		$sql .= "" . $data['partyId']   . ") ";
+
+		$result['status'] = $this->dbConn->db->query($sql);
+
+		$result  =  0;
+		return $result;
+	}
+
+	private function editData(Array &$data){
+
+		$sql = 'UPDATE ' . $this->table . ' SET ';
+		$sql .= "name = '"          . $data['name']     . "', ";
+		$sql .= "party_id = "       . $data['partyId']   . " ";
+		$sql .= 'WHERE id = '       . $data['id'];
+
+		$result['status'] = $this->dbConn->db->query($sql);
+
+		return $result;
+	}
+
+	private function deleteData(Array &$data){
+
+		$sql = 'DELETE FROM ' . $this->table . ' WHERE id = ' . $data['id'];
+
+		$result['status'] = $this->dbConn->db->query($sql);
+
+		return $result;
+	}
+
+
+	private function formatData($data){
+
+
+	}
+}
